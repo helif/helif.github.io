5c8e10297bf777560f65af01c1d577ca495591d1
helif <helif@users.noreply.github.com>
2018-09-10 02:12:06 +1000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,10 +2,9 @@
 
   "use strict";
   
-  var mainApp = angular.module("mainApp", []);
+  var mainApp = angular.module("mainApp", ['ngResource']);
 
   mainApp.controller("MainController", ['$scope', function($scope) {
-    $scope.message = "Flickr Search";
     $scope.tags = "";
     $scope.list = [];
   }]);
